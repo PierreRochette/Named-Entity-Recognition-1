076d126f66e614fd1c6790a5287804123764c7de
PierreRochette <pierre.rochette@epitech.eu>
2025-07-22 10:21:45 +0200

    first part OK

--- first.py
+++ first.py
@@ -5,7 +5,10 @@ texts = [
     "Lucas is going to the train station",
     "Bernard Arnault is the CEO of LVMH", 
     "Solomon Hykes is the guy behind Docker", 
-    "Pierre Rochette is the guy who followed this tutorial" 
+    "Pierre Rochette is the guy who followed this tutorial", 
+    "What is the price of four bananas ?", 
+    "How much are sixteen chairs ?", 
+    "Give me the value of five laptops."
 ]
 
 nlp = spacy.load('en_core_web_md') # Change this line if the model is different
@@ -13,14 +16,14 @@ nlp = spacy.load('en_core_web_md') # Change this line if the model is different
 ner_labels = nlp.get_pipe('ner').labels # Labels that are supported by the model by default
 print(ner_labels)
 
-categories = ["ORG", "PERSON", "LOC"]
+# categories = ["ORG", "PERSON", "LOC"]
 
 docs = [nlp(text) for text in texts]
 
 for doc in docs: 
     entities = []
     for ent in doc.ents: 
-        if ent.label_ in categories: 
-            entities.append((ent.text, ent.label_))
+        # if ent.label_ in categories: 
+        entities.append((ent.text, ent.label_))
 
     print(entities)
